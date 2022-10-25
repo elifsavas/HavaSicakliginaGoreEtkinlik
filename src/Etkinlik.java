5baaf57ee89aa9c5bceedb2a7e894ffe548da619
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-25 21:47:02 +0300

    Update Etkinlik.java

--- src/Etkinlik.java
+++ src/Etkinlik.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Etkinlik {
+import java.util.Scanner;
+
+public class Etkinlik {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hava Sıcaklığı Giriniz:");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz!");
+        } else if (heat <= 15) {
+            System.out.println("Sinemaya gidebilirsiniz!");
+        } else if (heat>25) {
+            System.out.println("Yüzebilirsiniz!");
+        }
+        if (heat >= 10 && heat < 25) {
+            System.out.println("Pikniğe gidebilirsiniz!");
+        }
+    }
 }
